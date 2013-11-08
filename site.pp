6fa3b4db7558c3ee969777b7f44a5e4f39bfa229
mil620 <ming_long@harvard.edu>
2013-11-07 21:25:26 -0500

    Puppet to install cas app test1

---
+++ site.pp
@@ -0,0 +1,13 @@
+node default {
+
+   tomcat::deployment { "cas-test-app1":
+      path => '/srv/puppet-tomcat-demo/java_src/cas-test-app1.war'
+   }
+
+   #tomcat::deployment { "SimpleServlet":
+   #   path => '/srv/puppet-tomcat-demo/java_src/SimpleServlet.war'
+   #}
+
+   # repeat as desired for different servlets ...
+
+}
